0383f1bc66b0e4da423051289bea2ada82b690f5
Trevor Stedman <trevor@we7.com>
2014-06-17 10:40:35 +0100

    exercise 1-12

---
+++ ex1-12.scm
@@ -0,0 +1,14 @@
+(define (pascal row col)
+(if(or
+      (= row 0)
+      (= col 0)
+      (= row col))
+    1
+    (+
+      (pascal
+        (- row 1)
+        (- col 1))
+      (pascal
+        (- row 1)
+         col))))
+
