cb46e080936bda6f3fefaf7d01ff051673f6733e
grahaminn <grahaminn@hotmail.com>
2014-06-17 11:13:54 +0100

    exercise 9

---
+++ iftest.scm
@@ -0,0 +1,19 @@
+(define (good-enough? guess x)
+      (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+      (average guess (/ x guess)))
+
+(define (average x y)
+      (/ (+ x y) 2))
+
+(define (new-if predicate then-clause else-clause)
+         (cond (predicate then-clause)
+               (else else-clause)))
+
+(define (sqrt-iter guess x)
+     (new-if (good-enough? guess x)
+             guess
+             (sqrt-iter (improve guess x) x)))
+
+(sqrt-iter 3 4)
