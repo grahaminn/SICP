079e316fa76db7133a973f047eedd95710935c5e
Random "Freedoomer" Hobo <kritzsie@thor.Home>
2014-07-15 02:23:24 +0100

    Added ex1-27.scm, fails on else

---
+++ ex1-27.scm
@@ -0,0 +1,25 @@
+(define (report-prime elapsed-time)
+  (display " *** ") (display elapsed-time))
+
+(define (dumb-fermat-test n)
+  (define (try-it a)
+    (if (< a n)
+      (if (= (expt a n) (modulo a n))
+        (try-it (+ a 1))
+      (else false))
+    (else n)))
+  (try-it 0))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((dumb-fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (start-fast-prime-test n start-time)
+  (if (fast-prime? n 1)
+    (report-prime (- (runtime) start-time))))
+
+(define (fast-timed-prime-test n)
+  (newline) (display n) (start-fast-prime-test n (runtime)))
+
+(map (lambda (x) (dumb-fermat-test x)) '(561 1105 1729 2465 2821 6601))
