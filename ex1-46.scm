58c37b1346c940e4583457117036bd8c40fc6205
grahaminn <grahami@blinkbox.com>
2014-08-05 22:20:32 +0100

    exercise 46

--- ex1-46.scm
+++ ex1-46.scm
@@ -1,1 +1,40 @@
-todo powerman
+(define (iterative-improve good-enough improve)
+  (lambda (guess) 
+    (if (good-enough guess) 
+      guess
+      ((iterative-improve good-enough improve) (improve guess))   
+    )
+  )
+)
+
+(define (sqrt-good-enough? x)
+  (lambda (guess) 
+    (< (abs (- (square guess) x)) 0.001)
+  )
+)
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (sqrt-improve x) 
+  (lambda (guess) (average guess (/ x guess)))
+)
+
+(define (sqrt x) 
+  ((iterative-improve (sqrt-good-enough? x) (sqrt-improve x)) 1.0)
+)
+
+(sqrt 4.0)
+
+(define tolerance 0.00001)
+
+(define (close-enough? f) (lambda (guess) (< (abs (- guess (f guess))) tolerance))) 
+
+(define (fixed-point f first-guess) ((iterative-improve (close-enough? f) f) first-guess))
+
+(define reciprocal (lambda (x) (average x (/ 1.0 x))))
+
+(fixed-point reciprocal 2.0) 
+
+(define (sqrt x) (lambda (y) (average y (/ x y))))
+
+(fixed-point (sqrt 9.0) 2.0)
