c8ef5fe4956c39d3abe4d83e25448a43e907cbe5
Innocent <grahamin@406c8f00b803.ant.amazon.com>
2014-10-14 23:02:43 +0100

    ch2 exercise 82

---
+++ ex2-83.scm
@@ -0,0 +1,21 @@
+(define (install-integer-package)
+  (define (tag x)
+    (attach-tag 'integer x))    
+  (put 'add '(integer integer)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(integer integer)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(integer integer)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(integer integer)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'integer
+       (lambda (x) (tag x)))
+
+  (define (raise x) (apply-generic 'raise x))
+
+  (put 'raise 'integer (lambda (x) (make-reational x 1)))
+
+  (put 'raise 'rational (lambda (x) (make-real (/ (numer x) (denom x)))))
+
+  (put 'raise 'real (lambda (x) (make-from-real-imag x 0))) 
