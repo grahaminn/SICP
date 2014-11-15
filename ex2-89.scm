b53cf7c9e2ece870e174144e2162ff574df762b0
grahaminn <grahami@blinkbox.com>
2014-11-15 21:25:44 +0000

    ch2 ex 89

--- ex2-89.scm
+++ ex2-89.scm
@@ -5,3 +5,6 @@
           (list (coeff term) term-list)
           (adjoin-term term (list 0 term-list)))))
 
+(define (first-term term-list) (make-term (- (length term-list) 1) (car term-list)))
+
+
