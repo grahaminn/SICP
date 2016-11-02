b829c65795b846d6f4d57857233257849747c766
grahaminn <grahami@blinkbox.com>
2016-11-02 23:39:44 +0000

    ex 30

---
+++ chapter3/ex30.scm
@@ -0,0 +1,19 @@
+(define (adder cin a b s cout)
+  (define (add-action-procedure)
+    (let 
+
+c1 a b s c2
+0  0 0 0 0 
+0  0 1 1 0
+0  1 0 1 0
+0  1 1 0 1
+1  0 0 1 0
+1  0 1 0 1
+1  1 0 0 1
+1  1 1 1 1
+
+c2 = a&b | (c1 & (a|b))
+s = (c1 & (a&b | !a&!b)) 
+
+
+
