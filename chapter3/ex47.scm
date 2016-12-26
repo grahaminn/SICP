5c2cc032ca3b695b4ff65fde5fb1e38c82108944
grahaminn <grahami@blinkbox.com>
2016-12-26 23:36:26 +0000

    ex47 with mutex

--- chapter3/ex47.scm
+++ chapter3/ex47.scm
@@ -1,5 +1,18 @@
 (define (make-semaphore n)
-  (let ((mutex (make-mutex)))
-    (define (the-semaphore s)
-      (cond ((eq? s 'acquire)
+	(let ((mutex (make-mutex)))
+		(define (the-semaphore s)
+			(cond 
+				((eq? s 'acquire) 
+					(mutex 'acquire)
+					(if (> n 0)
+						((set! n (- n 1)) (m 'release))
+						((m 'release) (the-semaphore 'acquire))))
+				((eq? s 'release)
+					(mutex 'acquire)
+					(set! n (+ n 1))
+					(mutex 'release))))))
+
+	
+																		
+
              
