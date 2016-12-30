057150bf2b11ad531923b36ec88a5e0681b1139b
grahaminn <grahami@blinkbox.com>
2016-12-30 01:32:57 +0000

    ex47 with check and set

--- chapter3/ex47.scm
+++ chapter3/ex47.scm
@@ -12,6 +12,22 @@
 					(set! n (+ n 1))
 					(mutex 'release))))))
 
+(define (make-semaphore2 n)
+	(let ((cell (list false))
+		(define (the-semaphore s)
+			(let ((count n)
+				(cond 
+					((eq? s 'acquire)
+						(if (test-and-set! cell)
+							(the-semaphore s)
+							(if (= count 0)
+								(begin (clear! cell) (the-semaphore s))
+								(begin (set! count (- count 1) (clear! cell))))))
+					((eq? s 'release) 
+						(if (test-and-set! cell)
+							(the-semaphore s)
+							(begin (set! count (+ count 1)) (clear! cell)))))))))))
+					  
 	
 																		
 
